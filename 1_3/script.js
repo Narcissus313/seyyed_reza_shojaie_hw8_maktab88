38a7a1cc3b903c2da30a415806531b83d1982d9d
narcissus313 <srz.shl@gmail.com>
2022-12-26 11:21:41 +0330

    1_3 ended

---
+++ 1_3/script.js
@@ -0,0 +1,11 @@
+let btnParentCopy = document.querySelector(".parent-copy");
+let btnChildCopy = document.querySelector(".child-copy");
+let targetText = document.querySelector(".target").firstChild.data;
+btnParentCopy.onclick = function () {
+	document.querySelector(".target").firstChild.data =
+		document.querySelector(".parent").textContent + targetText;
+};
+btnChildCopy.onclick = function () {
+	document.querySelector(".target").firstChild.data =
+		document.querySelector(".child").textContent + targetText;
+};
