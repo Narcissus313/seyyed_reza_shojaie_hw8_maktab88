430b39c99d86ff28beeb9ccdce7b810289d98a1a
narcissus313 <srz.shl@gmail.com>
2022-12-28 22:06:21 +0330

    sort ended

--- 1_7/script.js
+++ 1_7/script.js
@@ -1,55 +1,76 @@
+let newUserData = userData.slice();
+let params = Object.keys(newUserData[0]);
+
 let mainDiv = document.createElement("div");
-mainDiv.classList.add("main");
 mainDiv.style.width = "800px";
 mainDiv.style.margin = "30px auto";
 document.body.appendChild(mainDiv);
 
+let titleRow = document.createElement("tr");
+titleRow.appendChild(thMaker("Row"));
+
 let table = document.createElement("table");
-table.style.border = "1px solid black";
-table.style.margin = "auto";
-mainDiv.prepend(table);
+mainDiv.appendChild(table);
 
-let row = document.createElement("tr");
-row.style.backgroundColor = "black";
-row.style.color = "white";
-table.prepend(row);
-
+for (const key of Object.keys(newUserData[0])) {
+	titleRow.appendChild(thMaker(key));
+}
 function thMaker(text) {
 	let th = document.createElement("th");
 	th.style.padding = "4px 10px";
 	th.innerText = text;
 	return th;
 }
-
-row.appendChild(thMaker("Row"));
-row.appendChild(thMaker("Uid"));
-row.appendChild(thMaker("First Name"));
-row.appendChild(thMaker("Last Name"));
-row.appendChild(thMaker("City"));
-row.appendChild(thMaker("Postal Code"));
-row.appendChild(thMaker("Phone Numer"));
-row.appendChild(thMaker("Position"));
-
+function tdMaker(text) {
+	let td = document.createElement("td");
+	td.style.textAlign = "center";
+	td.style.padding = "4px 16px";
+	td.style.borderBottom = "1px solid black";
+	td.style.height = "38px";
+	td.innerText = text;
+	return td;
+}
 function userRowMaker(user) {
 	let tr = document.createElement("tr");
-	if (userData.indexOf(user) % 2 !== 0)
+	if (newUserData.indexOf(user) % 2 !== 0)
 		tr.style.backgroundColor = "rgb(228, 219, 219)";
-	let td = document.createElement("td");
-	td.style.padding = "4px 6px";
-	td.style.textAlign = "center";
-	td.style.borderBottom = "1px solid black";
-	td.innerText = userData.indexOf(user) + 1;
-	tr.prepend(td);
+	let td = tdMaker(newUserData.indexOf(user) + 1);
+	tr.appendChild(td);
 	for (const key of Object.keys(user)) {
-		let td = document.createElement("td");
-		td.style.textAlign = "center";
-		td.style.padding = "4px 16px";
-		td.style.borderBottom = "1px solid black";
-		td.innerText = user[key];
+		let td = tdMaker(user[key]);
 		tr.appendChild(td);
 	}
 	return tr;
 }
-for (const user of userData) {
+function tableSort(data, sortBy = "uid") {
+	switch (typeof newUserData[0][sortBy]) {
+		case "number":
+			data.sort((a, b) => a[sortBy] - b[sortBy]);
+			break;
+		case "string":
+			data.sort((a, b) => a[sortBy].localeCompare(b[sortBy]));
+			break;
+	}
+}
+function tableMaker() {
+	titleRow.style.backgroundColor = "black";
+	titleRow.style.color = "white";
+	table.style.border = "1px solid black";
+	table.style.margin = "auto";
+	table.appendChild(titleRow);
+}
+tableMaker();
+for (const user of newUserData) {
 	table.appendChild(userRowMaker(user));
+}
+for (let i = 0; i < params.length; i++) {
+	titleRow.children[i + 1].addEventListener("click", function () {
+		tableSort(newUserData, this.firstChild.data);
+		table.innerHTML = "";
+		tableMaker();
+		for (const user of newUserData) {
+			table.appendChild(userRowMaker(user));
+		}
+		return;
+	});
 }
