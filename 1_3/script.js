5a0ede151146ee360d4a2b8096490b5734d12fd6
narcissus313 <srz.shl@gmail.com>
2023-01-05 22:16:04 +0330

    1_3 corrected

--- 1_3/script.js
+++ 1_3/script.js
@@ -1,6 +1,7 @@
 let btnParentCopy = document.querySelector(".parent-copy");
 let btnChildCopy = document.querySelector(".child-copy");
 let targetText = document.querySelector(".target").firstChild.data;
+let childText = document.querySelector(".child").firstChild.data;
 btnParentCopy.onclick = function () {
 	document.querySelector(".target").firstChild.data =
 		document.querySelector(".parent").textContent + targetText;
